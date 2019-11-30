3e7910bad3fd641dacfd08107cb25a0079d2fc53
Simon Laux <mobile.info@simonlaux.de>
2019-11-30 04:11:13 +0100

    replace webpack by parcel

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,6 @@
-import "./styles/main.scss" 
+import "./styles/main.scss"
 
 import { h, render, Component } from 'preact';
 
-class App extends Component {
-  // Initialise our state. For now we only store the input value
-  state = { value: '' }
-
-  onInput = (ev:any) => {
-    // This will schedule a state update. Once updated the component
-    // will automatically re-render itself.
-    this.setState({ value: ev.target.value });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <form>
-          <input type="text" value={this.state.value} onInput={this.onInput} />
-          <button type="submit">Update</button>
-        </form>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById("app"));
+const App2 = <h1>Hello World!</h1>;
+render(App2, document.body);
