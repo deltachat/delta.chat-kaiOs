c9f698e4cad2cea0e775bba4367657b2013b31fa
Simon Laux <mobile.info@simonlaux.de>
2020-01-10 02:01:42 +0100

    mock: add message status

--- src/mock/deltachat.ts
+++ src/mock/deltachat.ts
@@ -1,7 +1,7 @@
 export class Context {
 
 
-    get chatList (){
+    get chatList() {
         return [
             new ChatListItem(10, "Peter Johnson", null, 3, "#ffbb00"),
             new ChatListItem(11, "Tanja Reis", null, 0, "#4f0000"),
@@ -11,26 +11,27 @@ export class Context {
         ]
     }
 
-    getAllMessagesForChat (chatId:number){
-        if(chatId == 14) {
+    getAllMessagesForChat(chatId: number) {
+        if (chatId == 14) {
             return [
-                new Message(20, 12, "Hello", Date.now() - 8000),
-                new Message(22, 1, "Hey", Date.now() - 7000),
-                new Message(23, 10, "welcome to our group", Date.now() - 6000),
-                new Message(27, 1, "thanks for creating it!", Date.now() - 3000),
+                new Message(20, 12, "Hello", Date.now() - 8000, MessageStatus.DELIVERED),
+                new Message(22, 1, "Hey", Date.now() - 7000, MessageStatus.READ),
+                new Message(23, 10, "welcome to our group", Date.now() - 6000, MessageStatus.DELIVERED),
+                new Message(27, 1, "thanks for creating it!", Date.now() - 3000, MessageStatus.PENDING),
             ]
         } else {
             return [
-                new Message(10, 10, "Hello", Date.now() - 7000),
-                new Message(12, 1, "Hey, how was your vacation?", Date.now() - 5000),
-                new Message(13, 10, "It was hot 😎", Date.now() - 4000),
-                new Message(17, 1, "❤️", Date.now() - 3000),
+                new Message(10, 10, "Hello", Date.now() - 7000, MessageStatus.DELIVERED),
+                new Message(12, 1, "Hey, how was your vacation?", Date.now() - 5000, MessageStatus.READ),
+                new Message(13, 10, "It was hot 😎", Date.now() - 4000, MessageStatus.DELIVERED),
+                new Message(17, 1, "❤️", Date.now() - 3000, MessageStatus.DELIVERED),
+                new Message(17, 1, "jo", Date.now() - 2900, MessageStatus.ERROR),
             ]
         }
     }
 
-    getContactById(contactId:number){
-        const mock_contact_db:{[key:number]:Contact} = {
+    getContactById(contactId: number) {
+        const mock_contact_db: { [key: number]: Contact } = {
             1: new Contact(1, "Me"),
             10: new Contact(10, "Felix"),
             12: new Contact(12, "Andrea"),
@@ -42,31 +43,41 @@ export class Context {
 
 export class ChatListItem {
 
-    constructor (
-        public ChatId:number,
-        public name:string,
-        public profileImage:string,
-        public unreadMessageCount:number,
-        public avatarColor:string
-    ){}
+    constructor(
+        public ChatId: number,
+        public name: string,
+        public profileImage: string,
+        public unreadMessageCount: number,
+        public avatarColor: string
+    ) { }
+}
+
+export enum MessageStatus {
+    UNKNOWN,
+    /** pending = currently sending */
+    PENDING,
+    DELIVERED,
+    READ,
+    ERROR,
 }
 
 
 export class Message {
-    constructor (
-        public messageId:number,
+    constructor(
+        public messageId: number,
         /** contact id of the author */
-        public authorId:number,
-        public text:string,
+        public authorId: number,
+        public text: string,
         public timestamp: number,
-        // todo (author and so on)
-    ){}
+        public status: MessageStatus,
+        // todo (misiing values)
+    ) { }
 
-    isOutgoing(){
+    isOutgoing() {
         return this.authorId === 1
     }
 
-    isIncomming(){
+    isIncomming() {
         return this.authorId !== 1
     }
 }
@@ -74,9 +85,9 @@ export class Message {
 
 
 export class Contact {
-    constructor (
-        public contactId:number,
-        public displayName:string,
+    constructor(
+        public contactId: number,
+        public displayName: string,
         // todo - add missing properties
-    ){}
+    ) { }
 }
