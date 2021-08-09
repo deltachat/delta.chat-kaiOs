6b07a3a5aa5d4c5108ba6bfc90cf0140493a838a
Simon Laux <mobile.info@simonlaux.de>
2021-08-09 18:35:32 +0200

    event logging

--- src/manager.ts
+++ src/manager.ts
@@ -2,6 +2,15 @@ import { Context } from './mock/deltachat'
 
 export const context = new Context()
 
-import { DeltaChat } from 'dc_cmd_api/src/deltachat'
+import { DeltaChat, Event } from 'dc_cmd_api/src/deltachat'
 
 export const dc_core = new DeltaChat('ws://localhost:8080/api_ws')
+
+
+function logEvent(logFn:(...args:any)=>void){
+    return (event:Event) => logFn(`[AC ${event.contextId}]`, event.field1, event.field2)
+}
+
+dc_core.on("WARNING", logEvent(console.warn))
+dc_core.on("ERROR", logEvent(console.error))
+dc_core.on("INFO", logEvent(console.info))
