7b43346a29220fe6568a75279c52420296c8b461
Simon Laux <mobile.info@simonlaux.de>
2021-08-11 00:37:20 +0200

    accept chat, sending and displaying messages

--- src/components/messageStatus.tsx
+++ src/components/messageStatus.tsx
@@ -1,18 +1,17 @@
-import { MessageStatus } from '../mock/deltachat'
-
-export function MessageStatusIcon({ status }: { status: MessageStatus }) {
+export function MessageStatusIcon({ status }: { status: number }) {
   switch (status) {
-    case MessageStatus.PENDING:
+    case 18 /* MsgOutPreparing */:
+    case 20 /* MsgOutPending */:
       return (
         <img src='../images/icons/sending.svg' class='status-icon sending' />
       )
-    case MessageStatus.DELIVERED:
+    case 26 /* MsgOutDelivered */:
       return (
         <img src='../images/icons/sent.svg' class='status-icon delivered' />
       )
-    case MessageStatus.READ:
+    case 28 /* MsgOutMdnRcvd */:
       return <img src='../images/icons/read.svg' class='status-icon read' />
-    case MessageStatus.ERROR:
+    case 24 /* MsgOutFailed */:
       return <img src='../images/icons/error.svg' class='status-icon error' />
     default:
       return <span class='status-icon'></span>
